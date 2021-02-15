b346cc3f3a694409b0543265c0f24a1c7db7f9a5
Getu Gebre <getu.gebre.gg@gmail.com>
2021-02-15 11:25:35 -0600

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,5 @@
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -6,8 +8,11 @@ function changeState(state, action){
       return state;
   }
 }
-
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+function render(){
+  document.body.textContent = state.count
+}
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
+render()
